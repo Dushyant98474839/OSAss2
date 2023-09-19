846d3d90252adc410552bf1a2e09105c150714a4
Dushyant98474839 <118445519+Dushyant98474839@users.noreply.github.com>
2023-09-19 13:35:26 +0530

    Update ass2qn3final.c

--- qn3/ass2qn3final.c
+++ qn3/ass2qn3final.c
@@ -1,1 +1,32 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/sched/signal.h> 
+#include <linux/moduleparam.h>
 
+MODULE_LICENSE("GPL"); 
+MODULE_AUTHOR("DUSHYANT"); 
+MODULE_DESCRIPTION("ASS2");
+
+static int __init display_init(void){ 
+	struct task_struct *str;
+	int n=0;
+	printk(KERN_INFO "LOADING..");
+	for_each_process(str){
+		//printk(KERN_ALERT "%d\n" +n);
+		if(str->state==TASK_RUNNING)
+		{
+			//printk(KERN_INFO "TT");
+			n++;
+		}
+		//printk(KERN_ALERT "%d\n" +n);
+	}
+	printk(KERN_INFO "COUNT: %d\n",n); 
+	return 0;
+}
+
+static void __exit display_cleanup(void){ 
+	printk(KERN_INFO "EXITING\n");
+}
+
+module_init(display_init);
+module_exit(display_cleanup);
